13ce5f5e061ed52645a1d5e531b78dbf0d715c83
Luis Gustavo <luisgustavordp1@gmail.com>
2024-10-28 19:25:31 -0300

    Initial Commit

---
+++ src/main/java/br/com/sistematizacao/pooIII/PooIiiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistematizacao.pooIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistematizacao/pooIII/PooIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistematizacao.pooIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
