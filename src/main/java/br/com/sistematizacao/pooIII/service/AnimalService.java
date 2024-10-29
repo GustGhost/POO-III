b2fa9b526d3fd019cabb4c6935f7216ed37f2d3e
Priscila Lessa <prillybsb@gmail.com>
2024-10-28 21:10:15 -0300

    adicionar service

--- src/main/java/br/com/sistematizacao/pooIII/service/AnimalService.java
+++ src/main/java/br/com/sistematizacao/pooIII/service/AnimalService.java
@@ -1,4 +1,49 @@
 package br.com.sistematizacao.pooIII.service;
 
+import br.com.sistematizacao.pooIII.entity.Animal;
+import br.com.sistematizacao.pooIII.exception.ResourceNotFoundException;
+import br.com.sistematizacao.pooIII.repository.AnimalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AnimalService {
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    public List<Animal> listarAnimais() {
+        return animalRepository.findAll();
+    }
+
+    public Animal cadastrarAnimal(Animal animal) {
+        return animalRepository.save(animal);
+    }
+
+    public Animal atualizarAnimal(Long id, Animal animalAtualizado) {
+        return animalRepository.findById(id)
+                .map(animal -> {
+                    animal.setNome(animalAtualizado.getNome());
+                    animal.setTipo(animalAtualizado.getTipo());
+                    animal.setIdade(animalAtualizado.getIdade());
+                    animal.setRaca(animalAtualizado.getRaca());
+                    animal.setStatus(animalAtualizado.getStatus());
+                    animal.setDescricao(animalAtualizado.getDescricao());
+                    animal.setImagemUrl(animalAtualizado.getImagemUrl());
+                    return animalRepository.save(animal);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado"));
+    }
+
+    public void removerAnimal(Long id) {
+        Animal animal = animalRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado"));
+        animalRepository.delete(animal);
+    }
+
+    public Animal buscarPorId(Long id) {
+        return animalRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Animal não encontrado"));
+    }
 }
